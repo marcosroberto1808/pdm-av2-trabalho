28e7f95fcb98ef0d6c397361777cda4d37018553
unknown <marcosroberto1808@gmail.com>
2019-06-02 16:14:35 -0300

    ajustes

--- App.js
+++ App.js
@@ -1,62 +1,13 @@
 import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { Home, Detalhes, AddItems, EditItems, ImageUpload, Loader } from './src/screens';
+import { createAppContainer } from 'react-navigation';
+
 
 import { View } from 'react-native';
 import Navigator from './src/navigators/Navigator1';
 
 console.disableYellowBox = true;
-// const App = createStackNavigator(
-//   {
-//     Home: {
-//       screen: Home,
-//       navigationOptions: {
-//         title: 'Home'
-//       }
-//     },
-//     Detalhes: {
-//       screen: Detalhes,
-//       navigationOptions: {
-//         title: 'Detalhes do produto'
-//       }
-//     },
-//     AddItems: {
-//       screen: AddItems,
-//       navigationOptions: {
-//         title: 'Adicionar produto'
-//       }
-//     },
-//     EditItems: {
-//       screen: EditItems,
-//       navigationOptions: {
-//         title: 'Editar produto'
-//       }
-//     },
-//     ImageUpload: {
-//       screen: ImageUpload,
-//       navigationOptions: {
-//         title: 'Enviar foto'
-//       }
-//     },
-//     Loader: {
-//       screen: Loader,
-//       navigationOptions: {
-//         title: 'Carregando foto'
-//       }
-//     }
-
-//   },
-//   {
-//     initialRouteName: 'Home'
-//   }
-// );
-
-// export default createAppContainer(App);
-
 
 const App = createAppContainer(Navigator);
-
-// export default App;
 
 export default () => (
   <View style={{ flex: 1, marginTop: 20 }}>
